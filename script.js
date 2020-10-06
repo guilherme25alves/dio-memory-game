35ce28a705f40ea191b0e56ec2f9fd6006398b9c
unknown <guilhermealves2596@gmail.com>
2020-10-05 23:07:22 -0300

    Adding the game interact to project

--- script.js
+++ script.js
@@ -32,7 +32,8 @@ let score = 0;
           lightColor(elementColor, Number(i) + 1); // Number trás um número que vai existir na nossa lista de Cores
      }      
  }
-]// Acende a próxima Cor
+
+ // Acende a próxima Cor
  let lightColor = (element, number) => {
      number = number * 500;
      setTimeout(() => {
@@ -47,7 +48,7 @@ let score = 0;
  let checkOrder = () => {
       for(let i in clickedOrder) {
            if(clickedOrder[i] != order[i]) {
-                lose();
+                gameOver();
                 break;
            }
       }
@@ -59,13 +60,63 @@ let score = 0;
 
 
  // Função para evento clique do usuário
- let check = (color) => {
+ let click = (color) => {
       clickedOrder[clickedOrder.length] = color;
       createColorElement(color).classList.add('selected');
 
       setTimeout(() => {
            createColorElement(color).classList.remove('selected');
-      })
+           checkOrder();
+      }, 250)
 
-      checkOrder();
+      
  } 
+
+ // Função que retorna a cor 
+ let createColorElement = (color) => {
+      if(color == 0) {
+           return green;
+      } else if(color == 1) {
+           return red;
+      } else if (color == 2) {
+           return yellow;
+      } else if( color == 3) {
+           return blue;
+      }
+ }
+
+ // Função para prózcimo nível do Jogo
+ let nextLevel = () => {
+      score++;
+      shuffleOrder();
+ }
+
+ // Função para quem perder o jogo 
+let gameOver = () => {
+     alert(`Pontuação: ${score}\nVocê perdeu o jogo\nClique em OK para iniciar um novo jogo! `);
+     order = [];
+     clickedOrder = [];
+
+     playGame();
+}
+
+// Função que inicia o jogo
+let playGame = () => {
+     alert(' Bem vindo ao DIO-Gênesis! Iniciando um novo jogo!');
+     score = 0;
+
+     nextLevel();
+}
+
+// Ativando eventos de click para as cores
+green.addEventListener('click', click(0));
+red.addEventListener('click', click(1));
+yellow.addEventListener('click', click(2));
+blue.addEventListener('click', click(3));
+
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
+
+playGame();
